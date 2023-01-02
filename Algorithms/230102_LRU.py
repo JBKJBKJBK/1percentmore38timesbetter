55e78349508551d8adc035b7ecbb3297e8439e5a
JBKJBKJBK <108387482+JBKJBKJBK@users.noreply.github.com>
2023-01-03 00:32:40 +0900

    Update 230102_LRU.py

--- Algorithms/230102_LRU.py
+++ Algorithms/230102_LRU.py
@@ -1,5 +1,5 @@
 cacheSize = 3
-cities = ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]
+cities = ["Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"]
 
 timeHit = 1
 timeMiss = 5
@@ -14,16 +14,20 @@ cacheCities = []
 
 for city in cities:
     if city in cacheCities:
-        time += 1
+        time += timeHit
         cacheCities.remove(city)
         cacheCities.append(city)
         print('Hit: ' , cacheCities)
     else:
-        time += 5
-        if len(cacheCities) >= 3:
+        time += timeMiss
+        # cacheSize = 0 인 경우 pop()에러 발생
+        if cacheSize == 0:
+            continue  
+        # 'cacheSize != 0 and' 작성 시, 29번째 줄 애매
+        elif len(cacheCities) >= cacheSize:
             cacheCities.pop(0)
         cacheCities.append(city)
         print('Miss: ' , cacheCities)
 
 print(time)
-print(cacheCities)
+# print(cacheCities)
