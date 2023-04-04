3fe9810eec37fc78f9bc6cc7427901dbb90937a9
buji-run <git.joe627@gmail.com>
2023-04-04 14:09:00 +0900

    런타임 에러 수정, 통과

--- Algorithms/230404_pickingDolls.py
+++ Algorithms/230404_pickingDolls.py
@@ -6,7 +6,7 @@ def pickDolls(board, moves):
     arrBoard = np.array(board)
     # print(arrBoard)
     # print(arrBoard[0, 3], arrBoard[2, 2]) # 0 5
-    
+
     result = []
     count = 0
     for move in moves:
@@ -19,18 +19,28 @@ def pickDolls(board, moves):
                 break
             # print('test2')
         lengthResult = len(result)
-        top1, top2 = result[lengthResult-1], 0
+# 런타임 에러
+# 0 0 0 0
+# 0 0 1 0
+# 0 2 4 1
+# 0 3 2 4
+# 처음부터 한 줄 전체 인형 없을 때 top1 값때문에 컴파일에러
+
+        top1, top2 = 0, 0
+        if lengthResult >= 1:
+            top1 = result[lengthResult-1]
         if lengthResult >= 2:
             top2 = result[lengthResult-2]
         # print(top1, top2)
-        if top1 == top2:
+        if top1 and top1 == top2:
             count += 2
             result = result[0:lengthResult-2]
         # print('test3 : ', result)
         
     return count
 
-board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
+# board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
+board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[0,2,4,4,2],[0,5,1,3,1]]
 moves = [1,5,3,5,1,2,1,4]
 
 print(pickDolls(board, moves))
