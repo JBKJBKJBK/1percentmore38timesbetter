f981ccf3e7475736618ff6009cddb042024c2528
buji-run <git.joe627@gmail.com>
2023-04-07 01:44:59 +0900

    4차 시도, replace 사용

--- Algorithms/230406_LV2_theSong.py
+++ Algorithms/230406_LV2_theSong.py
@@ -45,6 +45,7 @@ def duration(start, end):
 #     return listInfo[2], playedMelody
 
 # -------- 조건이 일치하는 음악이 여러개일때 재생시간 긴 것 -------- 3차 시도
+"""
 def playedInfo(info):
     listInfo = info.split(',')
     # print(listInfo)
@@ -63,8 +64,10 @@ def playedInfo(info):
             continue
         i += 1
         count += 1
-    # print(time, playedMelody, listInfo[3])
+        print('i = ', i, '/ count = ', count)
+    print(time, playedMelody, listInfo[3])
     return time, listInfo[2], playedMelody
+"""
 
 
 # -------- 1차 시도  --------
@@ -80,24 +83,82 @@ def playedInfo(info):
 #     return answer[0]
 
 # -------- 조건이 일치하는 음악이 여러개일때 재생시간 긴 것 -------- 3차 시도
+"""
 def solution(m, musicinfos):
     answer = [0]
     for info in musicinfos:
-        print('here: ' , playedInfo(info))
+        played = playedInfo(info)
+        print('here: ' , played)
         # test = m+'#'
         # print(test)
-        if m in playedInfo(info)[2] and m+'#' not in playedInfo(info)[2]:
+        if m in played[2] and m+'#' not in played[2]:
             print('1: ', answer)
-            if answer[0] < playedInfo(info)[0] :
+            if answer[0] < played[0] :
                 print('재생시간')
-                answer = playedInfo(info)
+                answer = played
                 print('2: ', answer)
     if answer == [0]:
         answer = [0, '(None)']
     return answer[1]
+"""
 
-m = "ABC"
-musicinfos = ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]
+# -------- 4차 시도 replace '#' --------
+def replaceHigh(m):
+    m = m.replace('C#', 'Z')
+    m = m.replace('D#', 'Y')
+    m = m.replace('F#', 'X')
+    m = m.replace('G#', 'W')
+    m = m.replace('A#', 'V')
+    return m
+
+def playedInfo(info):
+    listInfo = info.split(',')
+    time = duration(listInfo[0], listInfo[1])
+    
+    infoMelody = replaceHigh(listInfo[3])
+    L = len(infoMelody)
+    print(L, infoMelody)
+    playedMelody = ""
+    i = 0
+    while i < time :
+        # print(listInfo[3][i%L])
+        playedMelody += infoMelody[i%L]
+        i += 1
+    print(time, playedMelody, infoMelody)
+    return time, listInfo[2], playedMelody
+
+def solution(m, musicinfos):
+    answer = [0, '(None)']
+    m = replaceHigh(m)
+    for info in musicinfos:
+        played = playedInfo(info)
+        print('here: ' , played)
+        # answer = played
+        # test = m+'#'
+        # print(test)
+        if m in played[2] and m+'#' not in played[2]:
+            print('1: ', answer)
+            if answer[0] < played[0] :
+                print('재생시간')
+                answer = played
+                print('2: ', answer)
+    # if answer == '(None)':
+    #     answer = [0, '(None)']
+    return answer[1]
+
+
+m = "ABC#"
+# print(replaceHigh(m))
+# musicinfos = ["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]
 # musicinfos = ["13:00,13:05,WORLD,ABCDEF", "12:00,12:14,HELLO,C#DEFGAB"]
+# musicinfos = ["12:55,13:05,WORLD,ABC#DEF", "12:04,12:14,HELLO,C#DEFGAB"]
+# musicinfos = ["12:04,12:14,HELLO,C#DEFGAB", "12:55,13:05,WORLD,ABC#DEF"]
+musicinfos = ["12:04,12:14,HELLO,C#DEFGAB", "12:55,13:08,WORLD,ABCDEF#", "12:54,13:05,ans,DEFABC#"]
+
+# m = "ABDEFG"
+# musicinfos = ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]
+
 print(solution(m, musicinfos))
+
+
 
