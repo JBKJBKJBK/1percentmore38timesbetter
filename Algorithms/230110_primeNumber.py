96c401d75b85f2e11775b7e336baeb37f436d1c9
buji-run <git.joe627@gmail.com>
2023-04-07 14:43:35 +0900

    valueError해결,but 한문제 시간초과

--- Algorithms/230110_primeNumber.py
+++ Algorithms/230110_primeNumber.py
@@ -3,7 +3,7 @@ n, k = 437674, 3
 
 # ---- 230407  10진법 소수 확인 함수 추가 ---- #
 def checkPrime(num):
-    if num == 1:
+    if num == 1 or num == 0:
         return False
     for i in range(2, num):
         if num%i == 0:
@@ -31,24 +31,62 @@ def changeNumSys(n, k):
         n = quotient
 
     return kSys
+
+# 테스트 케이스
+n, k = n, k = 437664, 3
+n, k = n, k = 437, 2
+# print(changeNumSys(n, k))
     
+# ---- ValueError: invalid literal for int() with base 10: ''  ----
+# def splitNum(arr):
+#     primeNum = []
+#     tempNum = ''
+#     for num in arr:
+#         if num == 0:
+#             # print('tempNum = ', tempNum)
+#             if checkPrime(int(float(tempNum))) and (tempNum not in primeNum):
+#                 primeNum.append(tempNum)
+#                 #ValueError: invalid literal for int() with base 10: ''
+#             tempNum = ''
+#         else:
+#             tempNum += str(num)
+#         # print(tempNum, primeNum)
+#     primeNum.append(tempNum)
+#     return primeNum
 
 def splitNum(arr):
     primeNum = []
-    tempNum = ''
+    tempNum = 0
     for num in arr:
+        print('num : ', num, 'temp : ', tempNum)
         if num == 0:
-            print('tempNum = ', tempNum)
-            if checkPrime(int(tempNum)):
+            if checkPrime(tempNum) :
+                print("check and add")
                 primeNum.append(tempNum)
-                tempNum = ''
+                #ValueError: invalid literal for int() with base 10: ''
+            tempNum = 0
         else:
-            tempNum += str(num)
+            print('tempNum B = ', tempNum)
+            tempNum *= 10 
+            tempNum += num 
+            print('tempNum A = ', tempNum)
         print(tempNum, primeNum)
-    primeNum.append(tempNum)
-    return set(primeNum)
+        print('----------------')
+    if checkPrime(tempNum):
+        primeNum.append(tempNum)
+    return primeNum
 
-kSys = changeNumSys(n, k)
-print('kSys : ', kSys)
-print('primeNum : ', splitNum(kSys))
+def solution(n, k):
+    kSys = changeNumSys(n, k)
+    print('kSys : ', kSys)
+    primes = splitNum(kSys)
+    print('primeNum : ', primes)
+    return len(primes)
+
+# kSys = changeNumSys(n, k)
+# primes = splitNum(kSys)
+answer = solution(n, k)
+# print('kSys : ', kSys)
+# print('primeNum : ', primes)
+print(answer)
 
