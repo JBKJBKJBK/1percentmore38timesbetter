c2a34871191a66ad1b3bab89e6138d3309bd1b72
buji-run <git.joe627@gmail.com>
2023-04-07 11:07:20 +0900

    44번째 줄 간헐적 컴파일 에러

--- Algorithms/230110_primeNumber.py
+++ Algorithms/230110_primeNumber.py
@@ -1,15 +1,17 @@
-n, k = 110011, 10
+# n, k = 110011, 10
+n, k = 437674, 3
 
+# ---- 230407  10진법 소수 확인 함수 추가 ---- #
 def checkPrime(num):
+    if num == 1:
+        return False
     for i in range(2, num):
-        print(i)
         if num%i == 0:
             return False
     return True
 
-print(checkPrime(11))
-print(checkPrime(10))
-
+# print(checkPrime(11))
+# print(checkPrime(10))
 
 def changeNumSys(n, k):
     # print(n, k)
@@ -36,16 +38,17 @@ def splitNum(arr):
     tempNum = ''
     for num in arr:
         if num == 0:
-            if len(tempNum)>0 and tempNum != '1':
+            print('tempNum = ', tempNum)
+            if checkPrime(int(tempNum)):
                 primeNum.append(tempNum)
-            tempNum = ''
+                tempNum = ''
         else:
             tempNum += str(num)
-        # print(tempNum, primeNum)
+        print(tempNum, primeNum)
     primeNum.append(tempNum)
-    return primeNum
+    return set(primeNum)
 
 kSys = changeNumSys(n, k)
-# print(kSys)
-# print(splitNum(kSys))
+print('kSys : ', kSys)
+print('primeNum : ', splitNum(kSys))
 
