a312c801060095742b78efd9eddcb7094700e1cc
buji-run <git.joe627@gmail.com>
2023-04-24 18:56:12 +0900

    요금 결제 함수

--- Algorithms/230412_parkingFee.py
+++ Algorithms/230412_parkingFee.py
@@ -10,12 +10,30 @@ def calTime(T_in, T_out):
 # T_in, T_out = "10:30", "11:36"
 # print(calTime(T_in, T_out)
 
+# ---- 230424 ----
+def CalFee(time, fees):
+    default = fees[1]
+    additional = fees[3]
+
+    if time <= fees[0]:
+        return default
+    else :
+        temp = time - fees[0]
+        times = temp//fees[2]+1
+        # print(times)
+        return default + additional * times
+
+# fees = [180, 5000, 10, 600]
+# time = 190
+# print(CalFee(time, fees))
+
 def solution(fees, records):
     [T_basic, Fee_basic, T_unit, Fee_unit] = fees
     # print(f"기본시간은 {T_basic}.")
     # print("기본 요금은 {0}.".format(Fee_basic))
 
     info = {}
+    carNum_fee = {} 
     for record in records:
         record = record.split(" ")
         print(f'{record}')
@@ -29,6 +47,8 @@ def solution(fees, records):
             info[car_number] = [record[0]]
             T_in = info[car_number][0]
             print('1' , info[car_number], T_in)
+            print('!', bool(carNum_fee[car_number]))
+            # if carNum_fee[car_number] 
         elif record[2] == "OUT":
             print(type(info[car_number]))
             info[car_number].append(record[0])    # list에 append를 했는데 추가가 아니라 바뀌네...
@@ -37,6 +57,9 @@ def solution(fees, records):
             # print("In at {T_in}, Out at {T_out}".format(T_in, T_out))
             time = calTime(info[car_number][0], info[car_number][1])
             info[car_number].append(time)
+            # print(bool(info[car_number][3]))
+            if not info[car_number][3]:
+                info[car_number].append(0)
             print(info[car_number])
         print('--------------')
     print(info)
