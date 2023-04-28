7ae46edb42c65a517280a07d019163b57c123688
buji-run <git.joe627@gmail.com>
2023-04-28 16:25:48 +0900

    재귀호출 정확성 100%    효율성 0%

--- Algorithms/230428_robotJump.py
+++ Algorithms/230428_robotJump.py
@@ -2,18 +2,18 @@ def pow2(n):
     i = 1
     result = []
     while i*2 <= n:
-        print(i)
+        # print(i)
         result.append(i)
         i *= 2
-    return result
+    return sorted(result, reverse=True)
 
 # N = 20
 # print(pow2(N))
-
+"""
 def solution(n):
     # q, r = divmod(n,2)
     # print(f'q is {q} and r is {r}')
-    print('check', n//2)
+    print('check', n//2,)
 
     # 2의 거듭제곱이면 두배씩 순간이동만하면 됨
     print('pow2 : ', pow2(n))
@@ -28,6 +28,31 @@ def solution(n):
     print('add')
 
     return solution(n//2) + add
+"""
 
-N = 50
+def solution(n):
+    pow = pow2(n)
+    print(pow)
+    ans = 0
+
+    if n == 1 or n == 2:
+        return 1
+    if n == 3:
+        return 2
+
+    for num in pow:
+        q, r = divmod(n, num)
+        print(f'q is {q} and r is {r}')
+        if r == 0:
+            n = q
+            break
+        if num == 2 and r == 1:
+            ans = 1
+            n = q
+            break
+
+    print(ans , n)
+    return solution(n) + ans
+
+N = 5000
 print(solution(N))
