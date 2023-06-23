a9a74407e999199b9a22d99249f0bae8fb0d39b6
buji-learn <git.joe627@gmail.com>
2023-06-24 00:42:54 +0900

    Update 230623_Boost3-1Select.sql

--- SQL/230623_Boost3-1Select.sql
+++ SQL/230623_Boost3-1Select.sql
@@ -37,3 +37,21 @@ select ADDR, count(MEN_NO) as 회원수
     group by ADDR
     having count(MEN_NO) < 100
     order by count(MEM_NO) desc  /* desc : 내림차순, asc : 오름차순 */
+
+/*  GROUP BY
+    여러 열별로 그룹화 가능
+    group by에 있는 열들을 select에도 작성해야함
+*/
+select ADDR, GENDER, count(MEM_NO) as 회원수
+    from CUSTOMER
+    where ADDR in ('SEOUL', 'INCHEON')
+    group by ADDR, GENDER;
+
+/*
+    select      열1, 열2, 집계함수
+      from      테이블
+      where     조건
+      group by  (정렬할) 열1/ 열2
+      having    조건
+      order by  열1
+*/
