2f48a7e0782a003494b7560e59035e5d242f0c81
buji-learn <git.joe627@gmail.com>
2023-06-24 23:38:27 +0900

    Create 230624_Boost3-2JOIN.sql

---
+++ SQL/230624_Boost3-2JOIN.sql
@@ -0,0 +1,9 @@
+/* 관계 복습 */
+
+/*  
+    * ERM (Entity-Relationship Modeling)
+    개체-관계 모델링
+    관계형 DB에 테이블을 모델링할 때 사용
+    * 개체 (entity) : 하나 이상의 속성(Attribute)으로 구성된 객체
+    * 관계 (Relationship) : 속성(Entity)으로 구성된 객체
+*/
