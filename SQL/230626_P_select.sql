9394ed39830143a28eb1d1f28fbd6b858c5a1444
buji-learn <git.joe627@gmail.com>
2023-06-27 13:40:40 +0900

    Update 230626_P_select.sql

--- SQL/230626_P_select.sql
+++ SQL/230626_P_select.sql
@@ -9,6 +9,16 @@ select DR_NAME, DR_ID, MCDP_CD, ynd(HIRE_YMD)
 /* 12세 이하인 여자 환자 목록 출력하기 */    
 -- select PT_NAME, PT_NO, GEND_CD, AGE, TLNO
 select *
+-- 전화번호가 NULL이면 'NONE'으로 출력
+/* 1) 
+    case 
+        when tlno is null then 'none'
+        else tlno
+    end as tlno
+*/
+/* 2)
+    IF(TLNO IS NULL, 'NONE', TLNO) AS TLNO 
+*/
     from PATIENT
     where GEND_CD = 'W' and 
           AGE <= 12
