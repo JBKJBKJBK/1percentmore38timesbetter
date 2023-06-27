7eb27439ad53ed00e8ee254163c9554b23dba50a
buji-learn <git.joe627@gmail.com>
2023-06-27 10:57:46 +0900

    Create 230626_P_select.sql

---
+++ SQL/230626_P_select.sql
@@ -0,0 +1,15 @@
+/* 흉부외과 또는 일반외과 의사 목록 출력하기 */
+
+select DR_NAME, DR_ID, MCDP_CD, ynd(HIRE_YMD)  
+                                -- 날짜 코드 YMD만 나와야하는데 뒤에 시간없애야 함
+    from DOCTOR
+    where MCDP_CD in ('CS', 'GS')
+    order by HIRE_YMD desc, DR_NAME;
+
+/* 12세 이하인 여자 환자 목록 출력하기 */    
+-- select PT_NAME, PT_NO, GEND_CD, AGE, TLNO
+select *
+    from PATIENT
+    where GEND_CD = 'W' and 
+          AGE <= 12
+    order by AGE desc, PT_NAME asc;
