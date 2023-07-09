6e90032df143b6fadf03246c9b4d17e38a6b0d33
buji-learn <git.joe627@gmail.com>
2023-07-09 23:43:54 +0900

    Update 230705_Boost5-3.sql

--- NAVER_boostcourse/SQL/230705_Boost5-3.sql
+++ NAVER_boostcourse/SQL/230705_Boost5-3.sql
@@ -19,8 +19,25 @@ SELECT * FROM PRODUCT;  -- C
     )AS D
     GROUP BY mem_no;*/
 
--- TEST 
-SELECT *
-		, count(mem_no) AS 구매횟수
+-- TEST --
+
+/*
+SELECT mem_no
+		, datediff(MIN(order_date), MAX(order_date)) AS 테스트
 	FROM SALES
-    GROUP BY mem_no;  
+    GROUP BY mem_no;       
+-- 결과 : 마이너스 나옴
+*/
+    
+SELECT *
+		-- ,COUNT(order_no) AS 구매횟수
+	FROM (
+		SELECT mem_no
+				, MIN(order_date) AS 최초구매
+                , MAX(order_date) AS 최근구매
+                , COUNT(order_no) AS 구매횟수
+                , IF(COUNT(order_no)>1 and datediff(MIN(order_date), MAX(order_date))>=1 ,0,1) AS 재구매
+			FROM SALES
+        GROUP BY mem_no
+	) AS NW;
+    -- GROUP BY mem_no;   
