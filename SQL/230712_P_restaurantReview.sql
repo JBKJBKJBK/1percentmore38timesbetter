abf13c0be77d77f2d35efd4c492dc368928d0ea4
buji-learn <git.joe627@gmail.com>
2023-07-12 14:16:37 +0900

    프로그래머스 SQL 그룹별 조건에 맞는 식당 목록 출력하기

---
+++ SQL/230712_P_restaurantReview.sql
@@ -0,0 +1,50 @@
+SELECT T.MEMBER_NAME
+        , T.REVIEW_TEXT
+        , T.REVIEW_DATE
+        , T.작성수
+        FROM  (SELECT A.MEMBER_ID
+                    , A.MEMBER_NAME
+                    , B.REVIEW_TEXT
+                    , B.REVIEW_DATE
+                    , COUNT(B.REVIEW_TEXT) AS 작성수
+                FROM MEMBER_PROFILE AS A
+                LEFT JOIN REST_REVIEW AS B
+                ON A.MEMBER_ID = B.MEMBER_ID
+                GROUP BY A.MEMBER_ID
+               ) AS T
+        /*
+        WHERE T.작성수 = 3 이렇게 하면 실행됨
+        WHERE T.작성수 = MAX(T.작성수) 로 하면 에러.
+        "
+        SQL 실행 중 오류가 발생하였습니다.
+        Invalid use of group function
+        "
+        */
+        # ORDER BY
+
+/* TEST
+SELECT A.MEMBER_ID
+        , A.MEMBER_NAME
+        , B.REVIEW_TEXT
+        , B.REVIEW_DATE
+        , COUNT(B.REVIEW_TEXT) AS 작성수
+    FROM MEMBER_PROFILE AS A
+    LEFT JOIN REST_REVIEW AS B
+    ON A.MEMBER_ID = B.MEMBER_ID
+    GROUP BY A.MEMBER_ID;
+*/
+
+/* TEST 결과 : 3
+SELECT MAX(A.작성수)
+    FROM (
+            SELECT A.MEMBER_ID
+                , A.MEMBER_NAME
+                , B.REVIEW_TEXT
+                , B.REVIEW_DATE
+                , COUNT(B.REVIEW_TEXT) AS 작성수
+            FROM MEMBER_PROFILE AS A
+            LEFT JOIN REST_REVIEW AS B
+            ON A.MEMBER_ID = B.MEMBER_ID
+            GROUP BY A.MEMBER_ID
+    )AS A
+*/  
