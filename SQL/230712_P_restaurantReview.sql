3354bf9cf57f9d40fcb251bc548339978f8b7533
buji-learn <git.joe627@gmail.com>
2023-07-12 14:19:02 +0900

    서브쿼리 두번, 임시 테이블로 저장 예정

--- SQL/230712_P_restaurantReview.sql
+++ SQL/230712_P_restaurantReview.sql
@@ -20,7 +20,19 @@ SELECT T.MEMBER_NAME
         Invalid use of group function
         "
         */
-        # ORDER BY
+        WHERE T.작성수 = (SELECT MAX(A.작성수)
+                            FROM (
+                                    SELECT A.MEMBER_ID
+                                        , A.MEMBER_NAME
+                                        , B.REVIEW_TEXT
+                                        , B.REVIEW_DATE
+                                        , COUNT(B.REVIEW_TEXT) AS 작성수
+                                    FROM MEMBER_PROFILE AS A
+                                    LEFT JOIN REST_REVIEW AS B
+                                    ON A.MEMBER_ID = B.MEMBER_ID
+                                    GROUP BY A.MEMBER_ID
+                            )AS A)  -- 실행됨
+        -- ORDER BY
 
 /* TEST
 SELECT A.MEMBER_ID
