5125376f53d77aef6a53cc4e1dacd70163005c0f
buji-learn <git.joe627@gmail.com>
2023-07-14 08:41:03 +0900

    Create 230712_P_restaurantReview.sql

---
+++ SQL/230712_P_restaurantReview.sql
@@ -0,0 +1,67 @@
+CREATE TABLE JOINTABLE AS
+SELECT A.MEMBER_ID
+        , A.MEMBER_NAME
+        , B.REVIEW_TEXT
+        , B.REVIEW_DATE
+        , COUNT(B.REVIEW_TEXT) AS 작성수
+    FROM MEMBER_PROFILE AS A
+    LEFT JOIN REST_REVIEW AS B
+    ON A.MEMBER_ID = B.MEMBER_ID
+    GROUP BY A.MEMBER_ID;
+    
+-- 확인
+SELECT * FROM JOINTABLE;
+
+SELECT MEMBER_NAME
+        , REVIEW_TEXT
+        , REVIEW_DATE
+        , 작성수
+        FROM  JOINTABLE
+        /*
+        WHERE T.작성수 = 3 이렇게 하면 실행됨
+        WHERE T.작성수 = MAX(T.작성수) 로 하면 에러.
+        "
+        SQL 실행 중 오류가 발생하였습니다.
+        Invalid use of group function
+        "
+        */
+        WHERE T.작성수 = (SELECT MAX(A.작성수)
+                            FROM (
+                                    SELECT A.MEMBER_ID
+                                        , A.MEMBER_NAME
+                                        , B.REVIEW_TEXT
+                                        , B.REVIEW_DATE
+                                        , COUNT(B.REVIEW_TEXT) AS 작성수
+                                    FROM MEMBER_PROFILE AS A
+                                    LEFT JOIN REST_REVIEW AS B
+                                    ON A.MEMBER_ID = B.MEMBER_ID
+                                    GROUP BY A.MEMBER_ID
+                            )AS A)  -- 실행됨
+        -- ORDER BY
+
+/* TEST
+SELECT A.MEMBER_ID
+        , A.MEMBER_NAME
+        , B.REVIEW_TEXT
+        , B.REVIEW_DATE
+        , COUNT(B.REVIEW_TEXT) AS 작성수
+    FROM MEMBER_PROFILE AS A
+    LEFT JOIN REST_REVIEW AS B
+    ON A.MEMBER_ID = B.MEMBER_ID
+    GROUP BY A.MEMBER_ID;
+*/
+
+/* TEST 결과 : 3
+SELECT MAX(A.작성수)
+    FROM (
+            SELECT A.MEMBER_ID
+                , A.MEMBER_NAME
+                , B.REVIEW_TEXT
+                , B.REVIEW_DATE
+                , COUNT(B.REVIEW_TEXT) AS 작성수
+            FROM MEMBER_PROFILE AS A
+            LEFT JOIN REST_REVIEW AS B
+            ON A.MEMBER_ID = B.MEMBER_ID
+            GROUP BY A.MEMBER_ID
+    )AS A
+*/  
