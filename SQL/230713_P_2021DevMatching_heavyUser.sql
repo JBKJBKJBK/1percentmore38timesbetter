cea0ac5b7045b0343a88b43923fdbd24ead29c5a
buji-learn <git.joe627@gmail.com>
2023-07-13 13:54:29 +0900

    Create 230713_P_2021DevMatching_heavyUser.sql

---
+++ SQL/230713_P_2021DevMatching_heavyUser.sql
@@ -0,0 +1,11 @@
+SELECT A.*
+    FROM places AS A
+    -- WHERE B.등록수 > 1
+    LEFT JOIN (SELECT HOST_ID
+                        , COUNT(ID) AS 등록수
+                 FROM places
+                 GROUP BY HOST_ID
+              ) AS B
+    ON A.HOST_ID = B.HOST_ID
+    WHERE B.등록수 > 1
+    ORDER BY A.ID; 
