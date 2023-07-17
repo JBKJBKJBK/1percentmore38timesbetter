57e653dc66a846ed9beacd55f0ba31a83439c2e6
buji-learn <git.joe627@gmail.com>
2023-07-17 22:29:38 +0900

    프로그래머스 LV3 최고의 집합 1차

---
+++ Algorithms/230717_P_LV3_MaxTimes.py
@@ -0,0 +1,26 @@
+def solution(n, s):
+    if n == 1:
+        return [s]
+    
+    answer = []
+    standard = int(s/n)
+    print(f'standard is {standard}')
+    if standard == 0:
+        return [-1]
+    
+# while True:
+#     if abs(s-standard) < n:
+#         answer.append(s)
+#         break
+    temp = int(s/n)
+    print(f'temp is {temp}')
+    answer.append(temp)
+    s -= temp
+    print(f'now s get changed to {s}')
+    recurr = solution(n-1, s)
+    print(f'recurrsive list {recurr}')
+    
+    return answer + recurr
+
+n, s = 2, 9
+print(solution(n, s))
