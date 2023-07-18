376a5b312657cade7591607bd89af203d53100c6
buji-learn <git.joe627@gmail.com>
2023-07-18 12:17:35 +0900

    통과

--- Algorithms/230717_P_LV3_MaxTimes.py
+++ Algorithms/230717_P_LV3_MaxTimes.py
@@ -1,6 +1,28 @@
 def solution(n, s):
     if int(s/n) == 0:
         return [-1]
+    
+    # answer = [int(s/n)+1 for i in range(n)]
+    standard = int(s/n)+1
+    check = standard * n
+    # print(f'standard {standard} and check {check}')
+    
+    while True:
+        # print(check)
+        diff = check - s
+        if check == s:
+            break
+        elif check > s:
+            answer = [standard-1 for i in range(diff)] + [standard for i in range(n-diff)]
+        # print(f'{i}th answer is {answer}')
+        check = sum(answer)
+
+    return answer
+
+""" 2차시도 효율성
+def solution(n, s):
+    if int(s/n) == 0:
+        return [-1]
 
     answer = [int(s/n)+1 for i in range(n)]
     i = 0
@@ -10,6 +32,7 @@ def solution(n, s):
         i += 1
 
     return answer
+"""
 
 """ 1차시도 런타임 에러
 def solution(n, s):
