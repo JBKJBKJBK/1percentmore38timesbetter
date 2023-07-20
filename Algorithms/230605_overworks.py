3bc35a929f098e7ceb794b78e3c2bebc61145629
buji-learn <git.joe627@gmail.com>
2023-07-20 15:07:23 +0900

    list comprehension

--- Algorithms/230605_overworks.py
+++ Algorithms/230605_overworks.py
@@ -1,3 +1,23 @@
+# 230720
+def solution(n, works):
+    answer = 0
+    
+    while n > 0:
+        n -= 1
+        if max(works) == 0:
+            return 0
+
+        idx = works.index(max(works))
+        # print(idx)
+        works[idx] -= 1
+        # print(works)
+
+    # 변경 >> still 시간 초과
+    answer = sum([num**2 for num in works])
+    
+    return answer
+        
+""" 230605  >> 시간 초과
 def solution(n, works):
     answer = 0
     
@@ -15,7 +35,7 @@ def solution(n, works):
     for num in works:
         answer += num**2
     return answer
-
+"""
 
 n = 4
 works = [4, 3, 3]
