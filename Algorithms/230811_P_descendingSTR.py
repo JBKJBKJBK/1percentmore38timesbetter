0db87a7d45c4e1724ae549883f4babaf00b465f8
buji-learn <git.joe627@gmail.com>
2023-08-11 14:35:31 +0900

    다른 사람 풀이, join 사용

--- Algorithms/230811_P_descendingSTR.py
+++ Algorithms/230811_P_descendingSTR.py
@@ -20,39 +20,45 @@ def alphabetDict():
 # dictionary, numDictionary = alphabetDict()
 # print(dictionary, numDictionary)
 
-def solution(s):
-    # print('Z'>'a') >> FALSE
-    # print('A'<'a') >> TRUE
-    # print('A'<'z') >> TRUE
-    """
-    while True:
-        count = 0
+# def solution(s):
+#     # print('Z'>'a') >> FALSE
+#     # print('A'<'a') >> TRUE
+#     # print('A'<'z') >> TRUE
+#     """
+#     while True:
+#         count = 0
 
-        for i in range(len(s)-1):
-            print(s[i])
-            if s[i] < s[i+1]:
-                count += 1
-                s[i], s[i+1] = s[i+1], s[i]  #'str' object does not support item assignment
-                print(s)
-        if count == 0:
-            break
-    """
+#         for i in range(len(s)-1):
+#             print(s[i])
+#             if s[i] < s[i+1]:
+#                 count += 1
+#                 s[i], s[i+1] = s[i+1], s[i]  #'str' object does not support item assignment
+#                 print(s)
+#         if count == 0:
+#             break
+#     """
 
-    dictionary, numDictionary = alphabetDict()
-    # print(dictionary, numDictionary)
-    numbers = []
-    for i in range(len(s)):
-        numbers.append(dictionary[s[i]])
+#     dictionary, numDictionary = alphabetDict()
+#     # print(dictionary, numDictionary)
+#     numbers = []
+#     for i in range(len(s)):
+#         numbers.append(dictionary[s[i]])
     
-    print(numbers)
-    numbers = sorted(numbers, reverse=True)
-    print(numbers)
+#     # print(numbers)
+#     numbers = sorted(numbers, reverse=True)
+#     # print(numbers)
     
-    answer = ''
-    for num in numbers:
-        answer += numDictionary[num]
+#     answer = ''
+#     for num in numbers:
+#         answer += numDictionary[num]
         
-    return answer
+#     return answer
+
+
+## 다른 사람 풀이 
+
+def solution(s):
+    return ''.join(sorted(s, reverse=True))
 
 s = "Zbcdefg"
 print(solution(s))
